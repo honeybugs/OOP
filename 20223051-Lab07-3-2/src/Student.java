4bbd3d048b2804aa1af9cc6e81102bf85e9d62ec
honeybugs <39672943+honeybugs@users.noreply.github.com>
2022-10-13 14:13:13 +0900

    Add files via upload

---
+++ 20223051-Lab07-3-2/src/Student.java
@@ -0,0 +1,35 @@
+
+public class Student {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
